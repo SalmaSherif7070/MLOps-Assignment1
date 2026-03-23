8e96bf8117cf07721c6ebb1f1c178016b4639cbd
Salma Ali <202200622@zewailcity.edu.eg>
2026-03-23 21:10:45 +0200

    intial

---
+++ src/run_experiments.sh
@@ -0,0 +1,5 @@
+python src/train_mlflow.py --lr 0.1 --batch_size 2 --epochs 1
+python src/train_mlflow.py --lr 0.01 --batch_size 2 --epochs 1
+python src/train_mlflow.py --lr 0.001 --batch_size 2 --epochs 1
+python src/train_mlflow.py --lr 0.0002 --batch_size 2 --epochs 1
+python src/train_mlflow.py --lr 0.0002 --batch_size 4 --epochs 1
